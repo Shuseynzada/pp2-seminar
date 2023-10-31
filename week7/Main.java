5695845d8f721546acd1d8ff45a55032561434de
clear <git@github.com:Shuseynzada/git.git>
2023-10-31 12:54:12 +0400

    week7

--- week7/Main.java
+++ week7/Main.java
@@ -1,12 +1,6 @@
-import java.util.ArrayList;
-
 import week3.ex2.Invoice;
 public class Main {
     public static void main(String[] args) {
-        var list =5;
-        int lis=5;
-        System.out.println(length());
-
         //3
         Invoice a = new Invoice("S", "pd", 2, 2.0);
         System.out.println(a.getInvoiceAmount());
